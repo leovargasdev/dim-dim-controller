3c4df1349720afdfcd339453f3c1d13d7ee636a2
Leonardo Vargas <leu1607@hotmail.com>
2024-05-18 22:58:04 -0300

    feat: add outro formato de listagem

--- src/pages/transacoes/index.page.tsx
+++ src/pages/transacoes/index.page.tsx
@@ -2,11 +2,13 @@ import { NextPage } from 'next'
 import { TransactionsList, TransactionsResume } from './components'
 
 import styles from './styles.module.scss'
+import { TransactionsList2 } from './components/List2'
 
 const TransactionsPage: NextPage = () => (
   <div className={styles.container}>
     <TransactionsResume />
     {/* <TransactionsFilter /> */}
+    <TransactionsList2 />
     <TransactionsList />
   </div>
 )
